dd0a3aa401da6e7ba89852f00316df3e3d2b33d2
doanvtamhuynh <huynhdoan.2132@gmail.com>
2024-10-10 17:18:26 +0700

    feat: NetworkUtil

---
+++ app/src/main/java/com/example/getnetwork/NetworkUtil.java
@@ -0,0 +1,30 @@
+package com.example.getnetwork;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.NetworkInfo;
+import android.os.Build;
+
+public class NetworkUtil {
+
+    public static String getNetworkStatus(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        if (connectivityManager != null) {
+            Network activeNetwork = connectivityManager.getActiveNetwork();
+            if (activeNetwork != null) {
+                NetworkCapabilities networkCapabilities = connectivityManager.getNetworkCapabilities(activeNetwork);
+                if (networkCapabilities != null) {
+                    if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                        return "WiFi";
+                    } else if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                        return "4G/5G";
+                    }
+                }
+            }
+        }
+        return "No connection";
+    }
+}
